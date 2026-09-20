502e6cd851b5755a2a947f5483e15fb47df43b68
Devendra Patidar <en24cs3010351@medicaps.ac.in>
2026-09-21 00:08:06 +0530

    Time: 0 ms (100%), Space: 46.6 MB (41.32%) - LeetHub

---
+++ 1470-shuffle-the-array/1470-shuffle-the-array.java
@@ -0,0 +1,15 @@
+class Solution {
+    public int[] shuffle(int[] nums, int n) {
+        int ans[]= new int[nums.length];
+        int j=0;
+          for(int i=0;i<nums.length/2;i++){
+        
+            ans[j] = nums[i];
+            j++;
+
+            ans[j] = nums[i + n];
+                    j++;
+          }
+        return ans;
+    }
+}
